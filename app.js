a6cf67a7391d57d3855ef93d66ce39e132e9a014
Zues <aidensly16@gmail.com>
2026-01-01 16:19:37 -0600

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
       "Finalizing pick..."
     ];
 
-    var LIMIT_PER_DAY = 15;
+    var LIMIT_PER_DAY = 20;
     var COUNT_KEY = "zeusLiteDailyCount";
     var DATE_KEY = "zeusLiteDailyDate";
 
